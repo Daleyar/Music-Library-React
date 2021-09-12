27d849c1a9015f62e2675beed63cbb75f3259376
Ali Daley <ali.daley72@gmail.com>
2021-09-12 15:09:47 -0400

    Created a Component to Display the Data From the Django Rest API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
 import React, {Component} from "react";
+import axios from 'axios'
+import LibraryTable from "./LibraryTable/libraryTable";
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            allsongs: []
         };
     }
+
+    componentDidMount(){
+        this.getAllSongs();
+    }
+
+    async getAllSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        let songlist = response.data
+        this.setState({
+            allsongs: songlist
+        })
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <LibraryTable songs={this.state.allsongs}/>
             </div>
         );
     }
